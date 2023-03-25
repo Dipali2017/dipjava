6b2b9f80b2a5b2f1aec9e979d9c8cd601c0a0ed6
Dipali2017 <127392792+Dipali2017@users.noreply.github.com>
2023-03-25 12:16:18 +0530

    Add files via upload

---
+++ Continue.java
@@ -0,0 +1,16 @@
+package Java;
+
+public class Continue {
+
+	public static void main(String[] args) {
+		for (int i=1;i<=20 ;i++) {
+			if (i==3) {
+				continue;
+			}
+			System.out.println(i);
+			}
+		}
+
+	}
+
+
