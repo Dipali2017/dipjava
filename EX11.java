99523e52eb664e5adeb38c97ac9ddb37dc5db66c
Dipali2017 <127392792+Dipali2017@users.noreply.github.com>
2023-03-25 12:22:34 +0530

    Add files via upload

---
+++ EX11.java
@@ -0,0 +1,15 @@
+package Java;
+
+public class EX11 {
+
+	public static void main(String[] args) {
+		float f=0.4f;
+		if (f==0.4) {
+			System.out.println("Hello");
+		}
+		else
+			System.out.println("Hi");
+		}
+	}
+
+
