d87ebdba8a5251e9c364b988deac88359c024e6d
Dipali2017 <127392792+Dipali2017@users.noreply.github.com>
2023-03-25 12:26:36 +0530

    Add files via upload

---
+++ TestArray2.java
@@ -0,0 +1,13 @@
+package Java;
+
+import java.util.Arrays;
+
+public class TestArray2 {
+
+	public static void main(String[] args) {
+		int marks[]= {90,45,56,75,83};
+		System.out.println(Arrays.toString(marks));
+
+	}
+
+}
