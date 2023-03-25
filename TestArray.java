0d5ee2d1cc5ae567c1378a4311e268204195d442
Dipali2017 <127392792+Dipali2017@users.noreply.github.com>
2023-03-25 12:27:08 +0530

    Add files via upload

---
+++ TestArray.java
@@ -0,0 +1,14 @@
+package Java;
+
+public class TestArray {
+
+	public static void main(String[] args) {
+		int[] arr=new int[] {63,40,25,38,56,97};
+		for (int i:arr){
+			System.out.print( " "+i);
+			
+		}
+
+	}
+
+}
