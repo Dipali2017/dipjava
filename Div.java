b0d024671f1fcdac23cffff5dd6785c6b7fae627
Dipali2017 <127392792+Dipali2017@users.noreply.github.com>
2023-03-25 12:17:56 +0530

    Add files via upload

---
+++ Div.java
@@ -0,0 +1,12 @@
+package Java;
+
+public class Div {
+
+	public static void main(String[] args) {
+	        int a=45,b=2;
+	        int res=a/b;
+	        System.out.print("Division is "+res);
+
+	}
+
+}
