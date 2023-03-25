feeee0cc89cc3ec30d1c234e21c563b40c620b08
Dipali2017 <127392792+Dipali2017@users.noreply.github.com>
2023-03-25 12:17:28 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,18 @@
+package Java;
+
+public class Datatype {
+
+	public static void main(String[] args) {
+		byte b=24;
+		short s=1325;int i=21567803;
+		long l=126543870;
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(l);
+		
+		
+
+	}
+
+}
